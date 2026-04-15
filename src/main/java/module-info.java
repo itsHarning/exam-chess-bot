fdf77ddcf2e9ebf0e7f14c831ab36361db0e36df
hdtrap <peterpetertange@gmail.com>
2026-04-15 19:38:04 +0200

    Knight moves now added - seems to have some bugs though?

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module dk.harning.chess_demo {
     requires javafx.controls;
     requires javafx.fxml;
     //requires dk.harning.chess_demo;
+    //requires dk.harning.chess_demo;
 
     opens dk.harning.chess_demo.engine to javafx.fxml;
     exports dk.harning.chess_demo.engine;
