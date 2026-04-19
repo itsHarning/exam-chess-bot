dd3250120ce3ee53bcf629b0c45f67cac8661ca9
hdtrap <peterpetertange@gmail.com>
2026-04-19 21:36:39 +0200

    Implemented all standard movements except pawn. Castling and en passant not implemented.

--- src/main/java/dk/ek/chess_bot/engine/pieces/Piece.java
+++ src/main/java/dk/ek/chess_bot/engine/pieces/Piece.java
@@ -7,16 +7,56 @@ public class Piece {
 
     private static final int[] ROOK_DIRECTIONS = {16, 1, -16, -1};
     private static final int[] BISHOP_DIRECTIONS = {15, 17, -15, -17};
-    private static final int[] QUEEN_DIRECTIONS = {16, 1, -16, -1, 15, 17, -15, -17};
+    private static final int[] KING_QUEEN_DIRECTIONS = {16, 1, -16, -1, 15, 17, -15, -17};
+    private static final int[] KNIGHT_MOVES = {33, 31, 18, -14, -33, -31, -18, 14};
 
-    // Moves for rook, bishop and queen. int[] directions is the relevant list of directions from this class
-    public static int getSlidingMoves(boolean isWhite, int pos, int[] board, int direction, int[] buffer, int counter) {
+    public static int getMoves(boolean isWhite, int pos, int[] board, int[] buffer, int counter){
+        return switch (board[pos]) {
+            case 2, 10 -> getNonSlidingMoves(isWhite, pos, board, KNIGHT_MOVES, buffer, counter);
+            case 3, 11 -> getAllSlidingMoves(isWhite, pos, board, BISHOP_DIRECTIONS, buffer, counter);
+            case 4, 12 -> getAllSlidingMoves(isWhite, pos, board, ROOK_DIRECTIONS, buffer, counter);
+            case 5, 13 -> getAllSlidingMoves(isWhite, pos, board, KING_QUEEN_DIRECTIONS, buffer, counter);
+            case 6, 14 -> getNonSlidingMoves(isWhite, pos, board, KING_QUEEN_DIRECTIONS, buffer, counter);
+            default -> pos;
+        };
+    }
+
+    public static int getAllSlidingMoves(boolean isWhite, int pos, int[] board, int[] directions, int[] buffer, int counter) {
+        // Check each direction
+        for(int direction : directions)
+            // Get sliding moves
+            counter = getSlidingMoves(isWhite, pos, board, direction, buffer, counter);
+        return counter;
+    }
+
+    public static int getSlidingMoves(boolean isWhite, int pos, int[] board, int direction, int[] buffer, int counter){
+        int piece = board[pos];
+        int target = pos+direction;
+        // Check if target square is on the board
+        if(!isOffBoard(target)){
+            // If square is empty, encode as a valid move
+            if(board[target] == 0){
+                buffer[counter++] = IntegerEncoder.encodeMove(
+                        pos, target, piece, false, 0
+                );
+                // Call this method with target as new position
+                counter = getSlidingMoves(isWhite, target, board, direction, buffer, counter);
+                // If square is an enemy, encode it
+            } else if (isEnemy(isWhite,board[target])){
+                buffer[counter++] = IntegerEncoder.encodeMove(
+                        pos, target, piece, true, board[target]
+                );
+            }
+        }
+
+        return counter;
+    }
+
+    public static int getNonSlidingMoves(boolean isWhite, int pos, int[] board, int[] directions, int[] buffer, int counter){
         // Set what piece we are
         int piece = board[pos];
-        /* This method has to be called for each direction as to allow recursive calling
         // Check each direction
-        for(int dir : directions) */
-            // Set target square to avoid doing the math again and again
+        for(int direction : directions) {
             int target = pos+direction;
             // Check if target square is on the board
             if(!isOffBoard(target)){
@@ -25,8 +65,6 @@ public class Piece {
                     buffer[counter++] = IntegerEncoder.encodeMove(
                             pos, target, piece, false, 0
                     );
-                    // Call this method with target as new position
-                    counter = getSlidingMoves(isWhite, target, board, direction, buffer, counter);
                     // If square is an enemy, encode it
                 } else if (isEnemy(isWhite,board[target])){
                     buffer[counter++] = IntegerEncoder.encodeMove(
@@ -34,6 +72,8 @@ public class Piece {
                     );
                 }
             }
+        }
+
         return counter;
     }
 
@@ -55,8 +95,8 @@ public class Piece {
 
         int[] buffer = new int[100];
 
-        int amount = getSlidingMoves(false, 4, board, BISHOP_DIRECTIONS[1], buffer, 0);
-        //System.out.println(amount);
+        //int amount = getAllSlidingMoves(false, 1, board, BISHOP_DIRECTIONS, buffer, 0);
+        int nonSliding = getNonSlidingMoves(false, 1, board, KING_QUEEN_DIRECTIONS, buffer, 0);
 
         for(int i: buffer){
             System.out.println(IntegerEncoder.decodeFromSquare(i) + " -> " + IntegerEncoder.decodeToSquare(i));
